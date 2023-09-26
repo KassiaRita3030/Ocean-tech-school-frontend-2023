b46d5b58feb5a6128cb1fea6e287914021aa8922
Kassia Rita <116911439+KassiaRita3030@users.noreply.github.com>
2023-09-26 19:56:18 +0000

    Construção do Card

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import './App.css'
+import Card from './components/Card/Card'
+
+function App() {
+  const item1 = {
+    nome: 'Rick Sanchez',
+    ImageURL: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
+  }
+
+    const item2 = {
+      nome: 'Morty Smith',
+      ImageURL: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
+    }
+
+    const item3 = {
+      nome: 'Summer Smith',
+      ImageURL:'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
+    }
+
+    const itens = [item1, item2, item3]
+
+  return (
+    <>
+      {itens.map(function (item){
+        return <Card item={item}/>
+      })}
+    </>
+  )
+}
+
+export default App
